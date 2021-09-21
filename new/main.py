3ac6a227f0d7c03e07219013e2c810887d9264ad
Ben <ben.bartling@gmail.com>
2021-09-21 15:54:24 +0000

    pydantic new

---
+++ new/main.py
@@ -0,0 +1,25 @@
+from aiohttp.web import Application, json_response, middleware
+from aiohttp import web
+from aiohttp_pydantic import oas
+
+from model import Model
+from view import PetCollectionView, PetItemView
+
+
+@middleware
+async def pet_not_found_to_404(request, handler):
+    try:
+        return await handler(request)
+    except Model.NotFound as key:
+        return json_response({"error": f"Pet {key} does not exist"}, status=404)
+
+
+app = Application(middlewares=[pet_not_found_to_404])
+oas.setup(app, version_spec="1.0.1", title_spec="My App")
+
+app["model"] = Model()
+app.router.add_view("/pets", PetCollectionView)
+app.router.add_view("/pets/{id}", PetItemView)
+
+
+web.run_app(app)
---
+++ new/model.py
@@ -0,0 +1,55 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Friend(BaseModel):
+    name: str
+    age: int
+
+
+class Pet(BaseModel):
+    id: int
+    name: str
+    age: int
+    friends: Friend
+
+
+class Error(BaseModel):
+    error: str
+
+
+class Model:
+    """
+    To keep simple this demo, we use a simple dict as database to
+    store the models.
+    """
+
+    class NotFound(KeyError):
+        """
+        Raised when a pet is not found.
+        """
+
+    def __init__(self):
+        self.storage = {}
+
+    def add_pet(self, pet: Pet):
+        self.storage[pet.id] = pet
+
+    def remove_pet(self, id: int):
+        try:
+            del self.storage[id]
+        except KeyError as error:
+            raise self.NotFound(str(error))
+
+    def update_pet(self, id: int, pet: Pet):
+        self.remove_pet(id)
+        self.add_pet(pet)
+
+    def find_pet(self, id: int):
+        try:
+            return self.storage[id]
+        except KeyError as error:
+            raise self.NotFound(str(error))
+
+    def list_pets(self):
+        return list(self.storage.values())
