1d2d2f3d4b640ae3f2680a843007b1708b244e70
Ben <ben.bartling@gmail.com>
2021-09-21 19:54:28 +0000

    new pydantic

--- new/view.py
+++ new/view.py
@@ -9,24 +9,39 @@ from model import Error, Ven
 
 
 class VenCollectionView(PydanticView):
-    async def get(self, age: Optional[str] = None) -> r200[List[Ven]]:
+    async def get(self, ven_name: Optional[str] = None) -> r200[List[Ven]]:
         """
         List all vens
-
         Status Codes:
             200: Successful operation
+
+
+        No need for payload on GET
         """
         vens = self.request.app["model"].list_vens()
         return web.json_response(
-            [ven.dict() for ven in vens if age is None or age == ven.age]
+            [ven.dict() for ven in vens if ven_name is None or ven_name == ven.ven_name]
         )
 
     async def post(self, ven: Ven) -> r201[Ven]:
         """
         Add a new ven to the store
-
         Status Codes:
             201: Successful operation
+
+        {
+            "ven_name": "asdf",
+            "ven_id": "asdf",
+            "registration_id": "afds",
+            "utility_provder_info": {
+            "street_address": "asdf",
+            "city": "asf",
+            "state": "asf",
+            "utility_id": 44444
+            }
+        }	
+	
+
         """
         self.request.app["model"].add_ven(ven)
         return web.json_response(ven.dict())
@@ -58,6 +73,8 @@ class VenItemView(PydanticView):
     async def delete(self, ven_name: str, /) -> r204:
         """
         Deletes a ven
+
+        http://23.92.28.66:8080/vens/asfd
         """
         self.request.app["model"].remove_ven(ven_name)
         return web.Response(status=204)
